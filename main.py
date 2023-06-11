3f120e53faa8f2d9ceeb12b585c6647895cc4793
rjordell <rjordell@gmail.com>
2023-06-10 18:44:49 -0700

    fixed up code for part 1

--- main.py
+++ main.py
@@ -11,8 +11,9 @@ import pandas as pd
 
 def accuracy():
     #part 2:
-    #return leave_one_out_cross_validation()
-    return random.random()
+    #acc = leave_one_out_cross_validation()
+    acc = random.random()
+    return acc
 
 def leave_one_out_cross_validation(data, current_features, k):
     pass
@@ -21,16 +22,17 @@ def feature_search_demo(data):
     current_features = []
 
     for i in range(data):
-        print("On the", i+1, "th level of the search tree")
+        print("On the", i+1,"th level of the search tree")
         feature_to_add = []
         best_so_far_accuracy = 0
 
         for k in range(data):
-            if k in current_features:
-                print("--considering adding the", k+1, "feature")
-                accuracy = accuracy()
-                if accuracy > best_so_far_accuracy:
-                    best_so_far_accuracy = accuracy
+            if k not in current_features:
+                print("--Considering adding the", k+1, "feature")
+                acc = accuracy()
+
+                if acc > best_so_far_accuracy:
+                    best_so_far_accuracy = acc
                     feature_to_add = k
         
         current_features.append(feature_to_add)
@@ -44,4 +46,4 @@ scores = cross_val_score
 #need to find out how to read the file
 #df = pd.read_csv("small-test-dataset.txt", )
 
-feature_search_demo(int(input("enter number")))
+feature_search_demo(int(input("enter total number of features: ")))
